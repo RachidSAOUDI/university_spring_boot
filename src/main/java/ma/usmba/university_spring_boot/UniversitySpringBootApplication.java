6406743e6c09e7615a28d555224ce12d01b29ae0
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-01-29 00:53:05 +0100

    initial commit

---
+++ src/main/java/ma/usmba/university_spring_boot/UniversitySpringBootApplication.java
@@ -0,0 +1,13 @@
+package ma.usmba.university_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversitySpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/usmba/university_spring_boot/UniversitySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.usmba.university_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
